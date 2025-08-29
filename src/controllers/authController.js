c1eab43255b789e0ef4f7e0b0aa86d18bf352d87
liateg <liyategared7@gmail.com>
2025-08-30 02:39:46 +0300

    hash

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import {checkPasswor, hashPassword} from "src/utils/passwordUtils.js"
+import {checkPasswor, hash_Password} from "../utils/passwordUtils.js"
 
 const prisma = new PrismaClient();
 
 //Register user
 export const registerUser=async (req,res)=>{
     try{
-        const {name,email,pnumber,role,password}=req.body
-        const hashedPassword=hashPassword(password)
+        const {name,email,phoneNumber,role,password}=req.body
+        const passwordHash=await hash_Password(password)
+        // console.log(`${passwordHash} the tyoe ${typeof(passwordHash)}`)
         const user=await prisma.user.create({
-            data:{name,email,pnumber,role,hashedPassword}
+            data:{name,email,phoneNumber,role,passwordHash}
         })
      return res.status(201).json({ success:true,data:user});
     }catch(error){
-        return res.status(400).json({ success:fales,error: error.message});
+        return res.status(400).json({ success:false ,error: error.message});
     }
 }
+
