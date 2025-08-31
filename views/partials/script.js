6a04a14a9707f0dbb50f54fd7ddc7e9c3be236ea
liateg <liyategared7@gmail.com>
2025-08-31 19:26:37 +0300

    humburger

---
+++ views/partials/script.js
@@ -0,0 +1,7 @@
+ // Toggle menu for mobile
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+
+    hamburger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
