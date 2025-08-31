9d12db08450b916e6d883b41a24b5f8acd0a4ff2
liateg <liyategared7@gmail.com>
2025-08-31 17:19:42 +0300

    initial:

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,9 +2,9 @@ import express from "express"
 
 const router = express.Router();
 
-import {registerUser} from "../controllers/authController.js"
-import { validateRegister } from "../middleware/validationMiddleware.js"
+import {registerUser,loginUser} from "../controllers/authController.js"
+import { validateRegister,validateLogin } from "../middleware/validationMiddleware.js"
 
 router.post("/register", validateRegister, registerUser);
-
+router.post("/login", validateLogin, loginUser);
 export default router
