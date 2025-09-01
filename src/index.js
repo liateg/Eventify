925e0104af9ef0b5e3a7aa3fc66b3ef6bc258169
liateg <liyategared7@gmail.com>
2025-09-01 17:23:28 +0300

    flash added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import express from "express"
+import session from "express-session";
+import  flash from "connect-flash";
 import userRt from "./routes/authRoute.js"
+import { flashMiddleware } from "./middleware/flashMid.js";
+
+
+
 
 const app=express()
 const PORT = process.env.PORT || 3000;
 
+app.use(session({
+  secret:process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
 
+// Make flash messages available in EJS templates
+app.use(flashMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
+app.use(express.static("public"))
 app.use("/user",userRt)
-
+app.get("/",(req,res)=>{
+    res.render("home.ejs")
+})
+app.get("/register",(req,res)=>{
+    res.render("register.ejs")
+})
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
 app.listen(PORT,()=>{
     console.log(`Runneing on port ${PORT}`)
 })
