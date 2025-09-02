b3f651c0ab904163e2e37e343d3ef5339400d454
liateg <liyategared7@gmail.com>
2025-09-02 17:14:04 +0300

    dd2

--- src/routes/organizerRoute.js
+++ src/routes/organizerRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {postEvent} from "../controllers/organizerConttroller.js"
+import {postEvent ,editPost,getMyEvents,pauseEvent} from "../controllers/organizerConttroller.js"
 
-const router=express.router()
+const router=express.Router()
 
 router.post("/post",postEvent)
-
+router.post("/edit/:id",editPost)
+router.post("/hold",pauseEvent)
+router.get("/",getMyEvents)
 export default router
