c97843d41ef66ff64cb98f0e377451f584b72449
liateg <liyategared7@gmail.com>
2025-09-03 22:23:20 +0300

    forgot what i dd

--- src/routes/organizerRoute.js
+++ src/routes/organizerRoute.js
@@ -1,10 +1,10 @@
 import express from "express"
 import {postEvent ,editPost,getMyEvents,pauseEvent} from "../controllers/organizerConttroller.js"
-
+import {authenticateToken} from "../middleware/authorizationMiddleware.js"
 const router=express.Router()
 
-router.post("/post",postEvent)
-router.post("/edit/:id",editPost)
-router.post("/hold",pauseEvent)
-router.get("/",getMyEvents)
+router.post("/post",authenticateToken,postEvent)
+router.post("/edit/:id",authenticateToken,editPost)
+router.post("/hold",authenticateToken,pauseEvent)
+router.get("/",authenticateToken,getMyEvents)
 export default router
