752ae2e9a1df0795738fa6530c43ffd863de2ddc
liateg <liyategared7@gmail.com>
2025-09-06 15:17:46 +0300

    orgdf

--- src/routes/organizerRoute.js
+++ src/routes/organizerRoute.js
@@ -1,16 +1,31 @@
 import express from "express"
 import multer from "multer"
 import path from "path"
+import { fileURLToPath } from "url";
 import {postEvent ,editPost,getMyEvents,pauseEvent} from "../controllers/organizerConttroller.js"
 import {authenticateToken} from "../middleware/authorizationMiddleware.js"
-const storage=({
-  destination:(req, file, cb) => cb(null, __dirname, "public", "upload"),
+
+// get __filename and __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// const storage=multer.diskStorage({
+//   destination:(req, file, cb) => cb(null, __dirname, "public", "upload"),
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   }
+//  })
+
+//  const upload = multer({ storage });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(process.cwd(), "public", "upload")); // join into single path
+  },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
- })
+});
 
- const upload = multer({ storage });
+const upload = multer({ storage });
  const router=express.Router()
 
 
@@ -19,11 +34,12 @@ const storage=({
 router.post("/post",authenticateToken,upload.single("poster"),postEvent)
 router.post("/edit/:id",authenticateToken,editPost)
 router.post("/hold",authenticateToken,pauseEvent)
-router.get("/events",authenticateToken,getMyEvents)
+router.get("/myevents",authenticateToken,getMyEvents)
 router.get("/",authenticateToken,(req,res)=>{
     res.render("overview.ejs")
 })
 router.get("/create",authenticateToken,(req,res)=>{
-    res.render("creatEvent.ejs")
+    res.render("createEvent.ejs",{userId:req.user.id})
 })
+
 export default router
